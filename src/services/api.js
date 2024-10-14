05d496ddf2c524e0b91a433090e3683b350c1fcc
info <nasihz12345@gmail.com>
2024-10-14 15:39:02 -0400

    testing aws

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080', // Your backend URL
+  baseURL: 'http://18.219.43.93:8080', // Your backend URL
 });
 
 api.interceptors.request.use(
